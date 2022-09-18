1da06d0cd53350197d97abae731c7836b14e0c34
JayKearney <indi.mh@gmail.com>
2022-09-18 19:26:35 +0000

    add reserve a table form and link it to menu, fix static error and menu not linking

--- src/reservation/views.py
+++ src/reservation/views.py
@@ -1,3 +1,9 @@
+from .models import Reservation
 from django.shortcuts import render
 
+from reservation.models import Reservation
+
 # Create your views here.
+
+def reserve_table(request):
+    pass
