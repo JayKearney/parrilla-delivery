e5bcddd05d40c3d153069efc2bf6ea3f5ac2b7fe
JayKearney <indi.mh@gmail.com>
2022-09-18 17:39:25 +0000

    add breakfast, brunch and meals details and images

--- src/meals/models.py
+++ src/meals/models.py
@@ -31,5 +31,9 @@ class Meals(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.name
