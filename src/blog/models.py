c611e620dce6d09c0097f04606bd823c62cea556
JayKearney <indi.mh@gmail.com>
2022-09-19 19:59:54 +0000

    add blog post and fix category plural mistake

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
-    # author
+    author = models.ForeignKey(User , on_delete=models.CASCADE)
     # tags
-    #category
+    category = models.ForeignKey('Category'  ,null=True, on_delete=models.SET_NULL)
     created = models.DateTimeField(default=timezone.now)
 
 
