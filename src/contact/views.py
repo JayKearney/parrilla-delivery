bc5096bd4929c023b7906585d932bfd76c9e5344
JayKearney <indi.mh@gmail.com>
2022-09-24 20:16:03 +0000

    fix contact form

--- src/contact/views.py
+++ src/contact/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from django.core.mail import send_mail , BadHeaderError
+from django.http import HttpResponse  , HttpResponseRedirect
+from .forms import ContactForm
 # Create your views here.
 
+
 def send_email(request):
-    pass
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            phone = form.cleaned_data['phone']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+
+            try :
+                send_mail(name,message,from_email['test@test.com'])
+
+            except BadHeaderError:
+                return HttpResponse('invalid header') 
+
+    
+
+    else:
+        form = ContactForm()
+
+    context = {
+        'form' : form
+    }
+
+    return render(request , 'contact/contact.html' , context)
+
+
 
 def send_success(request):
     pass
--- src/parrilla/settings.py
+++ src/parrilla/settings.py
@@ -137,6 +137,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'testing@example.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+EMAIL_PORT = 1025
 
 
 # Default primary key field type
