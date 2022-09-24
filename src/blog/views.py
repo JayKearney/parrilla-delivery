bf0f78eff018a3ffb3f80da9f2dfa9672f8d232c
JayKearney <indi.mh@gmail.com>
2022-09-24 19:43:21 +0000

    add contact form, views and urls

--- src/blog/views.py
+++ src/blog/views.py
@@ -9,7 +9,7 @@ from django.core.paginator import Paginator
 
 def post_list(request):
     post_list = Post.objects.all()
-    paginator = Paginator(post_list, 1)
+    paginator = Paginator(post_list, 1) #List of articles per page
 
     page = request.GET.get('page')
     post_list = paginator.get_page(page)
--- src/parrilla/settings.py
+++ src/parrilla/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'reservation',
     'blog',
     'aboutus',
+    'contact',
 ]
 
 MIDDLEWARE = [
@@ -131,6 +132,11 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 # Default primary key field type
