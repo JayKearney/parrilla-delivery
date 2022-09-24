c83734deac44a5dd46f4e822e6d7392f5461f4d6
JayKearney <indi.mh@gmail.com>
2022-09-24 18:34:04 +0000

    add about us templates, why choose us and chefs images. Fix about us views

--- src/parrilla/urls.py
+++ src/parrilla/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('meals/' , include ('meals.urls' , namespace='meals')),
     path('blog/' , include('blog.urls' , namespace='blog')),
+    path('about-us/' , include('aboutus.urls' , namespace='aboutus')),
     path('reserve_table/' , include ('reservation.urls' , namespace='reservation')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
